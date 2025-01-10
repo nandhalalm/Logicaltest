01912987fec8e11076d10838f2158935d6d9c5b2
nandhalalm <nandhalal608@gmail.com>
2025-01-10 11:04:07 +0530

    test31

---
+++ logical test1/array evenindice/logical10-01-2025pattern.py
@@ -0,0 +1,10 @@
+
+n = int(input("Enter the number of rows: "))
+asciia=65
+for i in range(5):
+    for k in range(n-i-1):
+        print(" ",end='')
+    for j in range(i+1):
+         print(chr(asciia), end=' ')
+    asciia+=1 
+    print()
